fb52bbfefa321ffb8547273760f69f904d522d2b
izzi-cablevision/SFDX_izziVlocity <sfdx_izzivlocity@noreply.github.com>
2022-01-03 17:36:10 -0600

    remotes

--- .sh/temp.sh
+++ .sh/temp.sh
@@ -8,13 +8,13 @@ echo $BRANCH
 X=${TARGET:default}
 echo $X
 if [[ " $X " =~  'develop' ]]; then
-    VAR= git diff refs/heads/develop..$BRANCH --name-only
+    VAR= git diff  remotes/origin/develop..$BRANCH --name-only
 elif [[ " $X " =~  'release' ]]; then
-    VAR= git diff refs/heads/release..$BRANCH --name-only
-elif [[ " $X " =~ 'master' ]]; then
-    VAR= git diff refs/heads/master..$BRANCH --name-only
+    VAR= git diff remotes/origin/release..$BRANCH --name-only
+elif [[ " $X " =~  'master' ]]; then
+    VAR= git diff remotes/origin/master..$BRANCH --name-only
 # Siguiente condicion solo para fines de pruebas
-elif [[ " $X " =~ 'updateMetadata' ]]; then
-    VAR= git diff refs/heads/master..$BRANCH --name-only
+elif [[ " $X " =~  'updateMetadata' ]]; then
+    VAR= git diff remotes/origin/master..$BRANCH --name-only
 fi
 echo $VAR
