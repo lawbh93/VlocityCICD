d26a305f81086b511d560ff9668200a3480a3fca
lawbh93/VlocityCICD <VlocityCICD@noreply.github.com>
2022-01-03 16:30:33 -0600

    fix

--- .sh/temp.sh
+++ .sh/temp.sh
@@ -2,20 +2,18 @@
 echo "Get Diffs"
 git config --global user.email "VlocityCICD@noreply.github.com"
 git config --global user.name "lawbh93/VlocityCICD"
+CURREN_Branch= git branch -a
 
 X=${TARGET:default}
-
-CURRENT_Branch= git branch --show-current
-echo 'Current branch'
-echo ${CURRENT_Branch}
+echo $X
 if [[ " $X " =~  'develop' ]]; then
-    VAR= git diff --name-only remotes/origin/develop..${CURRENT_Branch}
+    VAR= git diff --name-only remotes/origin/develop..$CURREN_Branch > differenceFromDev.txt
 elif [[ " $X " =~  'release' ]]; then
-    VAR= git diff --name-only remotes/origin/release..${CURRENT_Branch}
+    VAR= git diff --name-only remotes/origin/release..$CURREN_Branch > differenceFromDev.txt
 elif [[ " $X " =~  'master' ]]; then
-    VAR= git diff --name-only remotes/origin/master..${CURRENT_Branch}
+    VAR= git diff --name-only remotes/origin/master..$CURREN_Branch > differenceFromDev.txt
 # Siguiente condicion solo para fines de pruebas
 elif [[ " $X " =~  'updateMetadata' ]]; then
-    VAR= git diff --name-only remotes/origin/master..${CURRENT_Branch}
+    VAR= git diff --name-only remotes/origin/master..$CURREN_Branch > differenceFromDev.txt
 fi
 echo $VAR
