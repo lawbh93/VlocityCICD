87784c25efb889a1ace5bc5c307b967e9db5853a
izzi-cablevision/SFDX_izziVlocity <sfdx_izzivlocity@noreply.github.com>
2022-01-03 17:12:49 -0600

    dix

--- .sh/temp.sh
+++ .sh/temp.sh
@@ -2,20 +2,19 @@
 echo "Get Diffs"
 git config --global user.email "VlocityCICD@noreply.github.com"
 git config --global user.name "lawbh93/VlocityCICD"
-CURREN_Branch= git branch -a
-Branch= ${BRANCHNAME:default}
+BRANCH=${BRANCHNAME:default}
 echo 'current'
-echo $Branch > TEXT
+echo $BRANCH
 X=${TARGET:default}
 echo $X
 if [[ " $X " =~  'develop' ]]; then
-    VAR= git diff --name-only remotes/origin/develop..remotes/origin/$Branch
+    VAR= git diff --name-only remotes/origin/develop..remotes/origin/$BRANCH
 elif [[ " $X " =~  'release' ]]; then
-    VAR= git diff --name-only remotes/origin/release..remotes/origin/$Branch
+    VAR= git diff --name-only remotes/origin/release..remotes/origin/$BRANCH
 elif [[ " $X " =~  'master' ]]; then
-    VAR= git diff --name-only remotes/origin/master..remotes/origin/$Branch
+    VAR= git diff --name-only remotes/origin/master..remotes/origin/$BRANCH
 # Siguiente condicion solo para fines de pruebas
 elif [[ " $X " =~  'updateMetadata' ]]; then
-    VAR= git diff --name-only remotes/origin/master..remotes/origin/$Branch
+    VAR= git diff --name-only remotes/origin/master..remotes/origin/$BRANCH
 fi
 echo $VAR
