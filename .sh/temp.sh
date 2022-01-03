4f726d4dc2da073ead06e874351be29cfb5d6d7a
izzi-cablevision/SFDX_izziVlocity <sfdx_izzivlocity@noreply.github.com>
2022-01-03 16:20:51 -0600

    show current

--- .sh/temp.sh
+++ .sh/temp.sh
@@ -5,17 +5,17 @@ git config --global user.name "lawbh93/VlocityCICD"
 
 X=${TARGET:default}
 echo $X
-CURREN_Branch= git branch --show-current
-
-echo 'current branch' ${git branch -a}
+CURRENT_Branch= git branch --show-current
+echo 'Current branch'
+echo $CURRENT_Branch
 if [[ " $X " =~  'develop' ]]; then
-    VAR= git diff --name-only remotes/origin/develop..$CURREN_Branch
+    VAR= git diff --name-only remotes/origin/develop..$CURRENT_Branch
 elif [[ " $X " =~  'release' ]]; then
-    VAR= git diff --name-only remotes/origin/release..$CURREN_Branch
+    VAR= git diff --name-only remotes/origin/release..$CURRENT_Branch
 elif [[ " $X " =~  'master' ]]; then
-    VAR= git diff --name-only remotes/origin/master..$CURREN_Branch
+    VAR= git diff --name-only remotes/origin/master..$CURRENT_Branch
 # Siguiente condicion solo para fines de pruebas
 elif [[ " $X " =~  'updateMetadata' ]]; then
-    VAR= git diff --name-only remotes/origin/master..$CURREN_Branch
+    VAR= git diff --name-only remotes/origin/master..$CURRENT_Branch
 fi
 echo $VAR
