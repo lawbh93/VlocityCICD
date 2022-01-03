626b2dc3198598a54b29009107320a212aac80d6
izzi-cablevision/SFDX_izziVlocity <sfdx_izzivlocity@noreply.github.com>
2022-01-03 16:18:36 -0600

    show current

--- .sh/temp.sh
+++ .sh/temp.sh
@@ -5,9 +5,9 @@ git config --global user.name "lawbh93/VlocityCICD"
 
 X=${TARGET:default}
 echo $X
-CURREN_Branch= git branch -a
+CURREN_Branch= git branch --show-current
 
-echo 'current branch' $CURREN_Branch
+echo 'current branch' ${git branch -a}
 if [[ " $X " =~  'develop' ]]; then
     VAR= git diff --name-only remotes/origin/develop..$CURREN_Branch
 elif [[ " $X " =~  'release' ]]; then
