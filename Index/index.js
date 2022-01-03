9efbced62a82fe02b77f492ed5d105e3f57bac80
izzi-cablevision/SFDX_izziVlocity <sfdx_izzivlocity@noreply.github.com>
2022-01-03 15:22:18 -0600

    fix

--- Index/index.js
+++ Index/index.js
@@ -6,20 +6,6 @@ try {
   const branchname = core.getInput('branch-name');
   console.log(`Hello ${branchname}!`);
   core.setOutput("branchHead", branchname);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  
-  var execProcess = require("./exec_process.js");
-    execProcess.result("bash temp.sh ${branchname}", function(err, response){
-    if(!err){
-        console.log(response);
-    }else {
-        console.log(err);
-    }
-    });
-
-
 } catch (error) {
   core.setFailed(error.message);
 }
--- Index/temp.sh
+++ Index/temp.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 echo "Get Diffs"
 
-X=${branchname:default}
+X=${TARGET:default}
 echo $X
-git config --global user.email "sfdx_izzivlocity@noreply.github.com"
-git config --global user.name "izzi-cablevision/SFDX_izziVlocity"
 CURREN_Branch= git branch -a
 VAR= git diff --name-only remotes/origin/$X..$CURREN_Branch
