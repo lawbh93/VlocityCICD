de5f00475ebb2ee6638e644e1e718b6c290896d2
izzi-cablevision/SFDX_izziVlocity <sfdx_izzivlocity@noreply.github.com>
2022-01-03 17:05:54 -0600

    fix

--- .sh/temp.sh
+++ .sh/temp.sh
@@ -3,19 +3,19 @@ echo "Get Diffs"
 git config --global user.email "VlocityCICD@noreply.github.com"
 git config --global user.name "lawbh93/VlocityCICD"
 CURREN_Branch= git branch -a
-Branch= ${CURRENT_Branch:default}
+Branch= ${BRANCHNAME:default}
 echo 'current'
 echo $Branch > TEXT
 X=${TARGET:default}
 echo $X
 if [[ " $X " =~  'develop' ]]; then
-    VAR= git diff --name-only remotes/origin/develop..$CURREN_Branch > differenceFromDev.txt
+    VAR= git diff --name-only remotes/origin/develop..$Branch
 elif [[ " $X " =~  'release' ]]; then
-    VAR= git diff --name-only remotes/origin/release..$CURREN_Branch > differenceFromDev.txt
+    VAR= git diff --name-only remotes/origin/release..$Branch
 elif [[ " $X " =~  'master' ]]; then
-    VAR= git diff --name-only remotes/origin/master..$CURREN_Branch > differenceFromDev.txt
+    VAR= git diff --name-only remotes/origin/master..$Branch
 # Siguiente condicion solo para fines de pruebas
 elif [[ " $X " =~  'updateMetadata' ]]; then
-    VAR= git diff --name-only remotes/origin/master..$CURREN_Branch > differenceFromDev.txt
+    VAR= git diff --name-only remotes/origin/master..$Branch
 fi
 echo $VAR
