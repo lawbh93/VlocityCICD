dc8eacff3e8001ad2826ac6bef951d5e704cbcf8
izzi-cablevision/SFDX_izziVlocity <sfdx_izzivlocity@noreply.github.com>
2022-01-03 15:52:54 -0600

    fix sh

--- .sh/temp.sh
+++ .sh/temp.sh
@@ -4,4 +4,15 @@ echo "Get Diffs"
 X=${TARGET:default}
 echo $X
 CURREN_Branch= git branch -a
-VAR= git diff --name-only remotes/origin/$X..$CURREN_Branch
+
+if [[ " $X " =~  'develop' ]]; then
+    VAR= git diff --name-only remotes/origin/develop..$CURREN_Branch
+elif [[ " $X " =~  'release' ]]; then
+    VAR= git diff --name-only remotes/origin/release..$CURREN_Branch
+elif [[ " $X " =~  'master' ]]; then
+    VAR= git diff --name-only remotes/origin/master..$CURREN_Branch
+# Siguiente condicion solo para fines de pruebas
+elif [[ " $X " =~  'updateMetadata' ]]; then
+    VAR= git diff --name-only remotes/origin/master..$CURREN_Branch
+fi
+echo $VAR
