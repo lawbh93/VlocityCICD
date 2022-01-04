d6a483f2e41f6f2aa593af7c6fe09ef58bc6f82b
lawbh93/VlocityCICD <VlocityCICD@noreply.github.com>
2022-01-04 10:47:45 -0600

    fix

--- .sh/temp.sh
+++ .sh/temp.sh
@@ -8,14 +8,14 @@ echo $BRANCH
 X=${TARGET:default}
 echo $X
 if [[ " $X " =~  'develop' ]]; then
-    VAR=git diff  remotes/origin/develop..$BRANCH --name-only DataPackMetadata
+    VAR=$(git diff  remotes/origin/develop..$BRANCH --name-only DataPackMetadata)
 elif [[ " $X " =~  'release' ]]; then
-    VAR=git diff remotes/origin/release..$BRANCH --name-only DataPackMetadata
+    VAR=$(git diff remotes/origin/release..$BRANCH --name-only DataPackMetadata)
 elif [[ " $X " =~  'master' ]]; then
-    VAR=git diff remotes/origin/master..$BRANCH --name-only DataPackMetadata
+    VAR=$(git diff remotes/origin/master..$BRANCH --name-only DataPackMetadata)
 # Siguiente condicion solo para fines de pruebas
 elif [[ " $X " =~  'updateMetadata' ]]; then
-    VAR=git diff remotes/origin/master..$BRANCH --name-only DataPackMetadata
+    VAR=$(git diff remotes/origin/master..$BRANCH --name-only DataPackMetadata)
 fi
 echo 'Differences start'
 echo $VAR
