b312a4e9e079efb34844a818f4b587d68a8deef9
lawbh93/VlocityCICD <VlocityCICD@noreply.github.com>
2022-01-04 15:11:23 -0600

    fix 1

--- Index/FindStrings.js
+++ Index/FindStrings.js
@@ -12,14 +12,33 @@ process.argv.forEach((val, index) => {
    }
    console.log('differences: '+differences);
  });
-
- for(let i=0;i<differences.length;i++) {
-   var str =differences[i] ;
-   var tokens = str.split("DataPackMetadata/Pricebook2/");
-   var drive = tokens[0];
-   var fileName = tokens[tokens.length - 1];
-   var len = drive.length + fileName.length;
-   //remove the current lenth and also space for 3 dots and 2 slashes
-     console.log(fileName);
-   }
  
+   fs.readFile('./FilePaths.json', 'utf8', (err, jsonString) => {
+      if (err) {
+          console.log("Error reading file from disk:", err)
+          return
+      }
+      try {
+          const DataPackInfo = JSON.parse(jsonString)
+          for(let i=0;i<differences.length;i++) {
+            var str =differences[i] ;
+            for(let j=0;j<DataPackInfo.length;j++) {
+               if(str[i].includes(DataPackInfo[j].FilePath)) {
+                  var newString=DataPackInfo[j].FilePath;
+                  var tokens = str.split(newString);
+
+            var drive = tokens[0];
+            var fileName = tokens[tokens.length - 1];
+            var len = drive.length + fileName.length;
+            //remove the current lenth and also space for 3 dots and 2 slashes
+              console.log(fileName);
+               }
+            }
+            }
+
+            
+  } catch(err) {
+          console.log('Error parsing JSON string:', err)
+      }
+  })
+  
