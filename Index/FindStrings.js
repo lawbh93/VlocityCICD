4f93d1a06dd628923e259872481057f39629d765
lawbh93/VlocityCICD <VlocityCICD@noreply.github.com>
2022-01-04 10:35:28 -0600

    new fix

--- Index/FindStrings.js
+++ Index/FindStrings.js
@@ -3,7 +3,6 @@ const github = require('@actions/github');
 
 var fs = require("fs")
 // Intitializing the readFileLines with filename
-process.argv.forEach(function (val, index, array) {
-   console.log(index + ': ' + val);
+process.argv.forEach((val, index) => {
+   console.log(`${index}: ${val}`);
  });
- 
