64c8c03c39645d4c6b89a7f1b45d4e7d409ea8de
lawbh93/VlocityCICD <VlocityCICD@noreply.github.com>
2022-01-04 15:26:15 -0600

    Fix

--- Index/FindStrings.js
+++ Index/FindStrings.js
@@ -1,47 +1,45 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 var fs = require("fs");
-var differences= [];
+var differences = [];
 
 // Intitializing the readFileLines with filename
 process.argv.forEach((val, index) => {
-   console.log(`${index}: ${val}`);
-   if(val.includes('DataPackMetadata')) {
-      differences.push(val);    
-   }
-   console.log('differences: '+differences);
- });
- 
-   fs.readFile('./Index/FilePaths.json', 'utf8', (err, jsonString) => {
-      if (err) {
-          console.log("Error reading file from disk:", err)
-          return
-      }
-      try {
-          const DataPackInfo = JSON.parse(jsonString)
-          for(let i=0;i<differences.length;i++) {
-            var str =differences[i];
-            console.log('str: '+str);
-            for(let j=0;j<DataPackInfo.length;j++) {
-               console.log('DataPackInfo[j].FilePath '+DataPackInfo[j].FilePath);
-               if(str.includes(DataPackInfo[j].FilePath)) {
-                  console.log('Si lo incluye');
-                  var newString=DataPackInfo[j].FilePath;
-                  var tokens = str.split(newString);
+  console.log(`${index}: ${val}`);
+  if (val.includes("DataPackMetadata")) {
+    differences.push(val);
+  }
+  console.log("differences: " + differences);
+});
 
-            var drive = tokens[0];
-            var fileName = tokens[tokens.length - 1];
-            var len = drive.length + fileName.length;
-            //remove the current lenth and also space for 3 dots and 2 slashes
-              console.log(fileName);
-               }
-            }
-            }
+fs.readFile("./Index/FilePaths.json", "utf8", (err, jsonString) => {
+  if (err) {
+    console.log("Error reading file from disk:", err);
+    return;
+  }
+  try {
+    const DataPackInfo = JSON.parse(jsonString);
+    for (let i = 0; i < differences.length; i++) {
+      var str = differences[i];
+      for (let j = 0; j < DataPackInfo.length; j++) {
+        if (str.includes(DataPackInfo[j].FilePath)) {
+          var newString = DataPackInfo[j].FilePath;
+          var tokens = str.split(newString);
 
-            
-  } catch(err) {
-          console.log('Error parsing JSON string:', err)
+          var drive = tokens[0];
+          var fileName = tokens[tokens.length - 1];
+          var len = drive.length + fileName.length;
+          console.log(fileName);
+          var FileShort=fileName.split('/');
+          FileshortName= FileShort[0];
+          var NPath=  DataPackInfo[j].FilePath+FileshortName;
+          console.log(NPath);
+
+        }
       }
-  })
-  
+    }
+  } catch (err) {
+    console.log("Error parsing JSON string:", err);
+  }
+});
