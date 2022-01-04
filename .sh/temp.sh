782178cb86ea8b2b9cd49d2e3afb2742c4eaec98
lawbh93/VlocityCICD <VlocityCICD@noreply.github.com>
2022-01-04 09:27:14 -0600

    export

--- .sh/temp.sh
+++ .sh/temp.sh
@@ -18,4 +18,4 @@ elif [[ " $X " =~  'updateMetadata' ]]; then
     VAR= git diff remotes/origin/master..$BRANCH --name-only DataPackMetadata
 fi
 echo $VAR
-export $VAR
+echo "DIFFS=$VAR" >> $GITHUB_ENV
