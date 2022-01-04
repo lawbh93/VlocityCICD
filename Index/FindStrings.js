33420f3c245d3e2a89c4c962e6aba543261db4a9
lawbh93/VlocityCICD <VlocityCICD@noreply.github.com>
2022-01-04 13:45:21 -0600

    test

--- Index/FindStrings.js
+++ Index/FindStrings.js
@@ -20,24 +20,6 @@ process.argv.forEach((val, index) => {
    var fileName = tokens[tokens.length - 1];
    var len = drive.length + fileName.length;
    //remove the current lenth and also space for 3 dots and 2 slashes
-   var remLen = maxLength - len - 5;
- 
-   //if remLen < 0, then it will over flow our maxLength to still maintain drive and filename
-   if (remLen > 0) {
-     //remove first and last elements from the array
-     tokens.splice(0, 1);
-     tokens.splice(tokens.length - 1, 1);
-     //recreate our path
-     var path = tokens.join("\\");
-     //handle the case of an odd length
-     var lenA = Math.ceil(remLen / 2);
-     var lenB = Math.floor(remLen / 2);
-     //rebuild the path from beginning and end
-     var pathA = path.substring(0, lenA);
-     var pathB = path.substring(path.length - lenB);
-     path = drive + "\\" + pathA + "..." + pathB + "\\" + fileName;
-     
-   } else {
      console.log(fileName);
    }
- }
+ 
