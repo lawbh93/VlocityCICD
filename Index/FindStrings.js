74585ebc086cad45ac3d278cdaffd6f2a9bffb2c
lawbh93/VlocityCICD <VlocityCICD@noreply.github.com>
2022-01-04 11:56:05 -0600

    fix new list

--- Index/FindStrings.js
+++ Index/FindStrings.js
@@ -2,13 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 var fs = require("fs");
-var differences= {};
+var differences= [];
 
 // Intitializing the readFileLines with filename
 process.argv.forEach((val, index) => {
    console.log(`${index}: ${val}`);
    if(val.includes('DataPackMetadata')) {
-      differences.push($(val));    
+      differences.push(val);    
    }
    console.log('differences: '+differences);
  });
